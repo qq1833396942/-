2fbb43fee92cb8d89fe687d76e1a0795ce1da39c
qq1833396942 <64786687+qq1833396942@users.noreply.github.com>
2020-07-02 08:55:49 +0800

    图书管理系统v2.1.cpp
    最新修改7.2

--- 图书管理系统.cpp
+++ 图书管理系统.cpp
@@ -2,21 +2,41 @@
 #include <list>
 #include <string>
 #include <fstream>
+#include <windows.h>
 #pragma warning(disable : 4996)
 using namespace std;
+void manageBook1();
 //书的结构体
+
 class user
 {
 public:
 	char accout[20]; //账户名
 	char password[20]; //账户密码
-	float balance; //余额
+	int number= 0;
 
+	char bookNam[5][20] = { {'\0'} ,{'\0'} ,{'\0'}, {'\0'} ,{'\0'} };
+
+
+
+	void showbill()  //借阅图书展示
+	{
+		for (int i = 0; i < 5; i++)
+		{
+
+			if (*bookNam[i] != '\0')
+			{
+				cout << "你借的书如下：" << endl;
+				cout << bookNam[i] << ' ';
+			}
+		}
+		cout << endl;
+	}
 };
 class manager
 {
 public:
-	char accout[20];
+
 	char password[20];
 };
 list<user> us; //存放用户数据的list容器
@@ -57,7 +77,6 @@ istream& operator>>(istream& in, book& data)
 //添加 书
 void Addbooks() //添加书籍
 {
-
 	book temData;
 	cin >> temData;
 	ob.push_back(temData);
@@ -126,8 +145,9 @@ void Findbook()
 
 }
 //借书
-void Borrowbooks()
+void Borrowbooks(list<user>::iterator ap)
 {
+
 	int flag = 0;
 	char bookname[51];
 	cout << "请输入你要借的书名:" << endl;;
@@ -135,27 +155,58 @@ void Borrowbooks()
 	list<book>::iterator it = ob.begin();
 	while (it != ob.end() && flag == 0)
 	{
+
 		if (!strcmp(bookname, it->book_name))
 		{
+
 			if (it->qcl >= 1)
 			{
-				cout << "借书成功！" << endl;
-				it->qcl--;
+				flag = 1;
+
+
+				if (ap->number >= 5)
+				{
+					cout << "你所借的书已满5本" << endl;
+					system("cls");
+					manageBook1();
+
+				}
+
+				else
+				{
+
+					cout << "借书成功！" << endl;
+					it->qcl--;
+					for (int i = 0; i < 5; i++)
+					{
+						if (*ap->bookNam[i] == '\0')
+						{
+							strcpy_s(ap->bookNam[i], bookname);
+						}
+						ap->number++;
+						return;
+					}
+				}
+
+
 			}
 			else
+			{
+				flag = 1;
 				cout << "书的库存量为0" << endl;
-			flag = 1;
-			break;
+				return;
+			}
+
 
 		}
-
 		++it;
 	}
+	
 	if (flag == 0) cout << "没有该书" << endl;
 
 }
 //还 书
-void Returnbooks()
+void Returnbooks(list<user>::iterator ap)
 {
 	int flag = 0;
 	char bookname[51];
@@ -164,26 +215,34 @@ void Returnbooks()
 	list<book>::iterator it = ob.begin();
 	while (it != ob.end() && flag == 0)
 	{
+		int i;
 		if (!strcmp(bookname, it->book_name))
 		{
-			cout << "还书成功！" << endl;
-			it->qcl++;
-			flag = 1;
 
-
-			break;
+			for (i = 0; i < 5; i++)
+			{
+				if (!strcmp(bookname, ap->bookNam[i]))
+				{
+					*ap->bookNam[i] = '\0';
+					ap->number--;
+					cout << "还书成功！" << endl;
+					it->qcl++;
+					return;
+				}
+			}
+			if (i == 5)
+			{
+				cout << "你没有借过这本书" << endl;
+				return;
+			}
 		}
-
 
 		it++;
 	}
-	if (flag == 0) cout << "没有该书" << endl;
 
 }
-//储存 书数据列表的类
-
-//数据存储
-void Savefile()  //保存到文件
+//保存到文件
+void Savefile()  
 {
 	list<book>::iterator it = ob.begin();
 	ofstream file("D:\\图书管理系统.txt");
@@ -200,6 +259,22 @@ void Savefile()  //保存到文件
 	}
 	cout << "数据保存完毕" << endl;
 	file.close();
+
+	list<user>::iterator it1 = us.begin();
+	ofstream file1("D:\\用户.txt");
+	if (!file1)
+	{
+		cout << "文件打开失败" << endl;
+	}
+
+	while (it1 != us.end())
+	{
+
+		file1 << it1->accout << '$' << *it1->password << '$' <<it1->number<< '$' << it1->bookNam[0] << '$' << it1->bookNam[1] << '$' << *it1->bookNam[2] << '$' << *it1->bookNam[3] << '$' << *it1->bookNam[4] << '$' <<  '\n' << endl;
+		it++;
+	}
+	cout << "数据保存完毕" << endl;
+	file1.close();
 }
 
 //从文件中读取数据
@@ -230,9 +305,11 @@ void Readfile()  //从文件读取数据
 
 		if (!ifile.get(ch))
 		{
-			cout << "文件已读完！"<< endl;
-			system("cls");
+			cout << "书的文件已读完！" << endl;
+			//system("cls");
+			ifile.close();
 			return;
+
 		}
 
 		while (ch != '$')//读取isbn
@@ -316,10 +393,11 @@ void Readfile()  //从文件读取数据
 		}
 		if (!ifile.get(ch))
 		{
-				cout << "文件已经读完！-------" << endl;
-				return;
+			cout << "文件已经读完！-------" << endl;
+			ifile.close();
+			return;
 		}
-	
+
 		book temp;
 		strcpy(temp.isbn, isbn.c_str());
 		strcpy(temp.book_name, book_name.c_str());
@@ -330,8 +408,14 @@ void Readfile()  //从文件读取数据
 		temp.price = strtod(price.c_str(), NULL);
 		ob.push_back(temp);
 	}
-	ifile.close();
 
+
+
+	
+}
+void Readfile1()
+{
+	char ch;
 	//读取用户数据
 	ifstream ifile1("D:\\用户.txt");
 
@@ -341,20 +425,28 @@ void Readfile()  //从文件读取数据
 		return;
 	}
 
-	//读取管理员数据
-	for (i = 0; i < 10; i++)
+	for (int i = 0; i < 10; i++)
 	{
 		string accout;  //用户名
 		string password; //密码
-		string balance;//余额
+		string number;//借书的数量
+		//存
+		string bookname1;
+		string bookname2;
+		string bookname3;
+		string bookname4;
+		string bookname5;
 
+		cout << '1';
 
 		if (!ifile1.get(ch))
 		{
-			cout << "文件已读完！" << endl;
-			system("cls");
+			cout << "用户文件文件已读完！" << endl;
+			//system("cls");
+			ifile1.close();
 			return;
 		}
+
 
 		while (ch != '$')//读取用户名
 		{
@@ -378,108 +470,144 @@ void Readfile()  //从文件读取数据
 			password += ch;
 			ifile1.get(ch);
 		}
-
+		cout << '2' << endl;
 		ifile1.get(ch);
-
-		while (ch != '\n')//读取价格
+		while (ch != '$')//读取借书的数量
 		{
 			if (ch == ' ')//跳过空格  
 			{
 				ifile1.get(ch);
 				continue;
 			}
-			balance += ch;
+			number += ch;
 			ifile1.get(ch);
 		}
-		if (!ifile1.get(ch))
+
+		ifile1.get(ch);
+		while (ch != '$')//读取书名1
 		{
-			cout << "文件已经读完！-------" << endl;
-			return;
-		}
-
-		user User;
-		strcpy(User.accout, accout.c_str());
-		strcpy(User.password, password.c_str());
-
-		temp.qcl = atof(balance.c_str());
-
-		us.push_back(User);
-	}
-	ifile1.close();
-
-	ifstream ifile2("D:\\管理员.txt");
-
-	if (!ifile2)
-	{
-		cout << "打开图书文件失败" << endl;
-		return;
-	}
-
-
-	for (i = 0; i < 100; i++)
-	{
-		string accout;  //用户名
-		string password; //密码
-
-		if (!ifile2.get(ch))
-		{
-			cout << "文件已读完！" << endl;
-			system("cls");
-			return;
-		}
-
-		while (ch != '$')//读取用户名
-		{
-			if (ch == ' ')
+			if (ch == ' ')//跳过空格  
 			{
-				ifile2.get(ch);
+				ifile1.get(ch);
 				continue;
 			}
-			accout += ch;
-			ifile2.get(ch);
+			bookname1 += ch;
+			ifile1.get(ch);
+		}
+		ifile1.get(ch);
+		while (ch != '$')//读取书名2
+		{
+			if (ch == ' ')//跳过空格  
+			{
+				ifile1.get(ch);
+				continue;
+			}
+			bookname2 += ch;
+			ifile1.get(ch);
+		}
+		ifile1.get(ch);
+		while (ch != '$')//读取书名3
+		{
+			if (ch == ' ')//跳过空格  
+			{
+				ifile1.get(ch);
+				continue;
+			}
+			bookname3 += ch;
+			ifile1.get(ch);
+		}
+		ifile1.get(ch);
+		while (ch != '$')//读取书名4
+		{
+			if (ch == ' ')//跳过空格  
+			{
+				ifile1.get(ch);
+				continue;
+			}
+			bookname4 += ch;
+			ifile1.get(ch);
 		}
 
 
 		ifile1.get(ch);
 
-		while (ch != '\n')//读取密码
+		while (ch != '\n')//读取书名5
 		{
 			if (ch == ' ')//跳过空格  
 			{
-				ifile2.get(ch);
+				ifile1.get(ch);
 				continue;
 			}
-			password += ch;
-			ifile2.get(ch);
+			bookname5 += ch;
+			ifile1.get(ch);
 		}
-		if (!ifile2.get(ch))
+
+		if (!ifile1.get(ch))
 		{
 			cout << "文件已经读完！-------" << endl;
+			ifile1.close();
 			return;
 		}
 
-		manager Manager
-		strcpy(Manager.accout, accout.c_str());
-		strcpy(Manager.password, password.c_str());
+		user User;
+		User.number = atoi(number.c_str());
+		strcpy_s(User.accout, accout.c_str());
+		strcpy_s(User.password, password.c_str());
+		strcpy_s(User.bookNam[0], bookname1.c_str());
+		strcpy_s(User.bookNam[1], bookname2.c_str());
+		strcpy_s(User.bookNam[2], bookname3.c_str());
+		strcpy_s(User.bookNam[3], bookname4.c_str());
+		strcpy_s(User.bookNam[4], bookname5.c_str());
+
+		us.push_back(User);
+	}
+	ifile1.close();
+}
+void Readfile2()
+{
+	char ch;
+	ifstream ifile2("D:\\管理员.txt");
+
+	if (!ifile2)
+	{
+		cout << "打开图书文件失败" << endl;
+
+		return;
+	}
+
+		string password; //密码
+
+		ifile2 >> password;
+			//getline(password);
+
+		//if (!ifile2.get(ch))
+		//{
+		//	cout << "管理员文件已读完！" << endl;
+		//	system("cls");
+		//	ifile2.close();
+		//	return;
+		//}
+		//while(ifile2.get(ch)!='$')
+		//{
+		//	password += ch;
+		//	ifile2.get(ch);
+		//}
+		//if (!ifile2.get(ch))
+		//{
+		//	cout << "文件已经读完！-------" << endl;
+		//	ifile2.close();
+		//	return;
+		//}
+
+		manager Manager;
+	
+		strcpy_s(Manager.password, password.c_str());
 
 
 		ma.push_back(Manager);
-	}
+	
 	ifile2.close();
 }
-//主菜单
-void menu()
-{
-	cout << "------------------【613图书管理系统】------------------------" << endl;
-	cout << "\t\t1.用户操作系统" << endl;
-	cout << "\t\t2.书库管理系统" << endl;
-	cout << "\t\t3.退出系统" << endl;
-
-	cout << "--------------------------------------------------------------" << endl;
-	cout << "\t\t输入选择（1-4）:";
-}
-
-
 //用户操作列表
 void manageBook1()
 {
@@ -488,9 +616,10 @@ void manageBook1()
 	cout << "\t\t2.借    书" << endl;
 	cout << "\t\t3.还    书" << endl;
 	cout << "\t\t4.查找书籍" << endl;
-	cout << "\t\t5.退出到主菜单" << endl;
+	cout << "\t\t5.个人信息" << endl;
+	cout << "\t\t6.退出登录" << endl;
 	cout << "--------------------------------------------------------------" << endl;
-	cout << "\t\t输入选择（1-5）:";
+	cout << "\t\t输入选择（1-6）:";
 }
 //管理员操作列表
 void manageBook2()
@@ -499,18 +628,28 @@ void manageBook2()
 	cout << "\t\t1.图书列表" << endl;
 	cout << "\t\t2.添加图书" << endl;
 	cout << "\t\t3.删除图书" << endl;
-	cout << "\t\t4.查找书籍" << endl;
-	cout << "\t\t5.退出到主菜单" << endl;
+	cout << "\t\t4.修改图书信息" << endl;
+	cout << "\t\t5.个人信息" << endl;
+	cout << "\t\t6.退出登录" << endl;
 	cout << "--------------------------------------------------------------" << endl;
-	cout << "\t\t输入选择（1-5）:";
+	cout << "\t\t输入选择（1-6）:";
+}
+
+//用户信息列表
+void userinformenu(list<user>::iterator it)
+{
+
+	cout << it->accout << "借书情况：" << endl;
+	it->showbill();
 }
 
 
 //用户交涉
-int usermanageKeyDown()
+int usermanageKeyDown(list<user>::iterator it)
 {
 	int userKey = 0;
 	cin >> userKey;
+	system("cls");
 	switch (userKey)
 	{
 	case 1:
@@ -520,21 +659,25 @@ int usermanageKeyDown()
 
 	case 2:
 		cout << "------------------【借    书】---------------" << endl;
-		Borrowbooks();
-		Savefile();
+		Borrowbooks(it);
 		break;
 	case 3:
 		cout << "------------------【还    书】---------------" << endl;
-		Returnbooks();
-		Savefile();
+		Returnbooks(it);
 		break;
 	case 4:
-		cout << "------------------【查    书】---------------" << endl;
+		cout << "------------------【查找书籍】---------------" << endl;
 		Findbook();
 		break;
 
 	case 5:
-
+		cout << "------------------【个人信息】---------------" << endl;
+		userinformenu(it);
+		system("pause");
+		system("cls");
+		break;
+	case 6:
+		Savefile();
 		break;
 	default:
 		break;
@@ -563,63 +706,25 @@ int manageBookKeyDown()
 		Savefile();
 		break;
 	case 4:
-		cout << "------------------【查    书】---------------" << endl;
+		cout << "------------------【修改图书信息】---------------" << endl;
 		Findbook();
 		break;
 
 	case 5:
-
+		cout << "------------------【个人信息】---------------" << endl;
+		while (1)
+		{
+			system("cls");
+		}
+		break;
+	case 6:
+		Savefile();
 		break;
 	default:
 		break;
 	}
 	return userKey;
 }
-//主菜单交互
-void keyDown()
-{
-	int userkey;
-	cin >> userkey;
-	switch (userkey)
-	{
-	case 1:
-		cout << "--------------【用户操作系统】----------------" << endl;
-		while (1)
-		{
-			manageBook1();
-			int flag = usermanageKeyDown();
-			if (flag == 5)
-				break;
-			system("pause");
-			system("cls");
-
-		}
-		break;
-	case 2:
-		cout << "--------------【书库管理系统】----------------" << endl;
-		while (1)
-		{
-			manageBook2();
-			int flag = manageBookKeyDown();
-			if (flag == 5)
-				break;
-			system("pause");
-			system("cls");
-
-		}
-		break;
-
-	case 3:
-		cout << "退出成功" << endl;
-		system("pause");//防止闪屏
-		exit(0);  //推出程序
-		break;
-	default:
-		cout << "输入错误，请重新输入" << endl;
-		break;
-	}
-}
-
 //账号登录界面
 int accoutmenu()
 {
@@ -627,7 +732,7 @@ int accoutmenu()
 	cout << "------------------【图书管理系统登录操作】------------------------" << endl;
 	cout << "\t\t1.用户身份登录" << endl;
 
-	cout<<"\t\t2.管理员身份登录" << endl;
+	cout << "\t\t2.管理员身份登录" << endl;
 	cout << "\t\t3.用户身份注册" << endl;
 
 	cout << "\t\t请输入您的选择：";
@@ -640,8 +745,7 @@ int accoutmenu()
 		case 2:system("cls"); return 2;
 		case 3:system("cls"); return 3;
 		default:
-			cout << "输入错误，请重新输入" << endl;
-			break;
+			cout << "输入错误，请重新输入:" << endl;
 		}
 		cin >> a;
 	}
@@ -649,30 +753,71 @@ int accoutmenu()
 
 }
 
+
+//用户操作列表
+void userinforBook()
+{
+	cout << "------------------【用户个人信息】------------------------" << endl;
+	cout << "\t\t1.余额查询" << endl;
+	cout << "\t\t2.流水查询" << endl;
+	cout << "\t\t3.修改密码" << endl;
+	cout << "\t\t4.返回菜单" << endl;
+	cout << "--------------------------------------------------------------" << endl;
+	cout << "\t\t输入选择（1-4）:";
+}
+//管理员操作列表
+void managerinforBook()
+{
+	cout << "------------------【用户个人信息】------------------------" << endl;
+	cout << "\t\t1.余额查询" << endl;
+	cout << "\t\t2.流水查询" << endl;
+	cout << "\t\t3.修改密码" << endl;
+	cout << "\t\t4.返回菜单" << endl;
+	cout << "--------------------------------------------------------------" << endl;
+	cout << "\t\t输入选择（1-4）:";
+}
+
+//用户信息
+
+
+
+
 //账号注册登陆交互
-void log(int accoutmenu())
+void log(int a)
 {
 	char accout[20];
 	char password[20];
-	float balance
-	user User;
-	manager Man;
+	//user User;
+	//manager Man;
 	int flag = 0;
-	int x = accoutmenu();
+	int x = a;
 	if (x == 1)
 	{
 		cout << "请输入您的用户的用户名以及密码:";
 		cin >> accout >> password;
 		while (1)
 		{
+
 			list<user>::iterator it = us.begin();
+
 			while (it != us.end() && flag == 0)
 			{
 				if (!strcmp(it->accout, accout))
 				{
 					if (!strcmp(it->password, password))
 					{
-						flag = 1;
+						cout << "用户" << accout << "登录成功" << endl;
+						while (1)
+						{
+							manageBook1();
+							int flag3 = usermanageKeyDown(it);
+							if (flag3 == 6)
+							{
+								system("pause");
+								system("cls");
+								return;
+							}
+						}
 					}
 					else
 					{
@@ -682,36 +827,41 @@ void log(int accoutmenu())
 							cin >> password;
 							if (!strcmp(it->password, password))
 							{
-								flag = 1;
+								cout << "用户" << accout << "登录成功" << endl;
+								while (1)
+								{
+									manageBook1();
+									int flag3 = usermanageKeyDown(it);
+									if (flag3 == 6)
+									{
+										system("pause");
+										system("cls");
+										return;
+									}
+								}
 							}
 							system("cls");
 						}
 					}
+					it++;
 				}
-				it++;
 			}
-			if (flag == 1)
-			{
-				cout << "管理员" << accout << "登录成功" << endl;
+
 			}
-			else
-			{
-				cout << "未找到该账户请重新输入" << endl;
-			}
-			cin >> accout >> password;
 		}
-	}
+	
 	else if (x == 2)
 	{
-		cout << "请输入您的管理员用户名以及密码:";
-		cin >> accout >> password;
+		cout << "请输入您的管理员密码:";
+		cin >> password;
+		list<manager>::iterator it1 = ma.begin();
 		while (1)
 		{
-			list<manager>::iterator it1 = ma.begin();
+
 			while (it1 != ma.end() && flag == 0)
 			{
-				if (!strcmp(it1->accout, accout))
-				{
+				
+				
 					if (!strcmp(it1->password, password))
 					{
 						flag = 1;
@@ -722,54 +872,82 @@ void log(int accoutmenu())
 						{
 							cout << "请重新输入密码:";
 							cin >> password;
-							if (!strcmp(it->password, password))
+							if (!strcmp(it1->password, password))
 							{
 								flag = 1;
 							}
 							system("cls");
 						}
 					}
-				}
+				
 				it1++;
 			}
 			if (flag == 1)
 			{
 				cout << "管理员" << accout << "登录成功" << endl;
+				while (1)
+				{
+					manageBook2();
+					int flag2 = manageBookKeyDown();
+					if (flag2 == 6)
+					{
+						system("pause");
+						system("cls");
+						return;
+					}
+				}
 			}
-			else
-			{
-				cout << "未找到该账户请重新输入" << endl;
-			}
-			cin >> accout >> password;
+			
+			
+				
+			
+		
 		}
 
 	}
+
 	else if (x == 3)
 	{
-		cout << "请输入您要注册的用户名以及密码和充值金额"<<endl;
-		cin >> accout >> password >> balance;
+		cout << "请输入您要注册的用户名以及密码：" << endl;
+		cin >> accout >> password;
+
 		ofstream file("D:\\用户.txt");
-		if (!file)
+		if (!file.is_open())
 		{
 			cout << "打开文件失败" << endl;
 		}
 
-		file << accout << '$' << password >> '$' << balance << '\n' << endl;
+		file << accout << '$' << password << '$' << '0' << '$' << '\0' << '$' << '\0' << '$' << '\0' << '$' << '\0' << '$' << '\0' <<  '\n' << endl;
+		file.close();
+		user User;
+
+		strcpy_s(User.accout, accout);
+		strcpy_s(User.password, password);
+
+		us.push_back(User);
+		
+		cout << "注册成功" << endl;
+		Sleep(1000);
+		system("cls");
+		return;
+
 	}
+
+	else return;
 }
+
 int main()
 {
 	Readfile();
-	accoutmenu();
-	logaccoutmenu());
-//	从文件中读取文件
+	Readfile1();
+	Readfile2();
+
+
 	while (1)
 	{
-
-		menu();
-		keyDown();
+		log(accoutmenu());
 		system("pause");
-		system("cls");//清屏作用
+		system("cls");
 	}
 
 	system("pause");
